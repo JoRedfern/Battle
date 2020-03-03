2aaab57498fc697929431db89ed504e6e4182ec3
Jo Redfern <mrsjoredfern@yahoo.co.uk>
2020-03-03 16:28:42 +0000

    added Attack button

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature "Enter names" do
   scenario "submitting names" do
-    visit("/")
-    fill_in :player_1_name, with: "Jo"
-    fill_in :player_2_name, with: "Cat"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content "Jo vs. Cat"
   end
 end
