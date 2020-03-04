08970b6770a43883d61b565c1d193574a4a9e167
Jo Redfern <mrsjoredfern@yahoo.co.uk>
2020-03-04 15:57:30 +0000

    created game class

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra/base"
 require_relative "lib/player.rb"
+require_relative "lib/game.rb"
 class Battle < Sinatra::Base
   enable :sessions
   get '/' do
@@ -18,7 +19,7 @@ class Battle < Sinatra::Base
   get "/attack" do
     @player_1 = $player_1
     @player_2 = $player_2
-    @player_1.attack(@player_2)
+    Game.new.attack(@player_2)
     erb :attack
   end
 run! if app_file == $0
